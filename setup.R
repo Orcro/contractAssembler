b58ea16e88940644276746ac8fdd37c1d3e81230
Alexander Murphy <alexander.murphy@orcro.co.uk>
2022-03-14 13:20:10 +0000

    different development env

--- setup.R
+++ setup.R
@@ -2,20 +2,23 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# don't execute this script every deployment! jump ahead to line 
+
+# don't execute this script every deployment! jump ahead to line 18
 
 # required to connect to shinyapps.io (not necessary if self hosted?)
 install.packages("rsconnect")
 install.packages("shiny")
 
+
+# deploy contractAssembler to  https://galacticalex.shinyapps.io/contractassembler/
+library(rsconnect)
+
 # run w/ credentials from web dashboard
 rsconnect::setAccountInfo(name = 'galacticalex', 
                           token = '<TOKEN>', 
                           secret = '<SECRET>')
-
-# deploy contractAssembler to  https://galacticalex.shinyapps.io/contractassembler/
-library(rsconnect)
-deployApp("~/Software/contractAssembler")
+localLocation = "."
+deployApp(localLocation)
 
 # run locally
 library(shiny)
