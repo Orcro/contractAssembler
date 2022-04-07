000bd3393cdb97fb203d9f8d60bc2e6d82cb1812
Alexander Murphy <alexander.murphy@orcro.co.uk>
2022-04-07 13:57:33 +0100

    refactor of server.R and ui.R for 0.1.1

--- SRC/server.R
+++ SRC/server.R
@@ -1,204 +1,146 @@
+
 # SPDX-FileCopyrightText: 2022 Orcro Ltd. <team@orcro.co.uk>
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# shiny!
-library(shiny)
 
-# Server definition
+# server.R
+
+
+# Server definition.
+# To aid maintenance, code is written in this order:
+# - Hard-coded values, followed by
+# - Reactive value, followed by
+# - Logic functions
+# - Render functions
 shinyServer(function(input, output) {
-  
-  
-  # hard-coded text on page:
-  output$csvTitle = renderText({
-    "<h3>Input Schema:</h3>"
-  })
-  # testing only, remove?
-  #output$contractFieldsTitle = renderText({
-  #    "<h3>Contract Fields:</h3>"
-  #})
-  output$tempTitle = renderText({
-    "Title Field"
-  })
-  output$widgetsTitle = renderText({
-    "<h3>Contract Widgets:</h3>"
-  })
-  output$outputTitle = renderText({
-    "<h3>Contract Output:</h3>"
-  })
-  output$blankSpace = renderText({
-  "<h3>  </h3>"
-  })
-  
-  
-  # a list to hold useful things
-  ui.list = list()
-  
-  
-  # switch responds to schema names
-  responsive.Widget = function(type, display, id) {
     
-    #id = as.character(id)
-    #output$id = renderText({ display })
     
-    switch (type,
-            qbox = textInput(inputId = id, label = display), 
-            title_text = NULL,
-            c_text = NULL
-    )
-  }
-  
-  # can these two be combined safely?
-  contract.Text = function(type, display) {
+    # 
+    # Hard-coded values #############################
+    # 
     
-    #id = as.character(id)
-    #output$id = renderText({ display })
+    # Default filename for download
+    download.filename = "export_contract.txt"
     
-    switch (type,
-            qbox = NULL, 
-            title_text = NULL,
-            c_text = display
-    )
-  }
-  
-  
-  # display the imported schema
-  output$userInput = renderTable({
-    req(input$csvUpload)
-    read.csv(file = input$csvUpload$datapath)
-  })
-  
-  
-  # user uploaded schema
-  inputSchema = reactive({
-    read.csv(file = input$csvUpload$datapath)
-  })
-  
-  outputText = reactive({
-    req(input$csvUpload)
-        t.out = unlist(list_of_inputs())
-        #paste0(t.out[6], " ", t.out[1])
-        t.index = unlist(list_of_fields())
-        
-     a = t.out[t.index] # this works!
-    b = unlist(mapply(contract.Text, 
-                  unlist(inputSchema()[, "Type"]), 
-                  unlist(inputSchema()[, "Display"]))) 
-    c(a, b)
-  })
-  
-  # downloadable contract
-  output$downloadContract = downloadHandler(
-    filename = "export_contract.txt", 
-    content = function(file) { write(paste0(outputText(), sep = "\n"), file = file) }
-  )
-  
-  # the fields on the contract
-  #contract.Fields = reactive({
-  #    unlist(inputSchema()[, "Type"])
-  #})
-  
-  
-  ## testing only, remove?
-  #output$contractFields = renderText({
-  #    tryCatch(
-  #        {
-  #            paste0(unlist(inputSchema()[, "Type"]), collapse = ", ")
-  #        }, 
-  #        error = function(cond){
-  #            # is this scrappy? :-)
-  #        })
-  #})
-  
-  
-  output$theForm = renderUI({
-    tryCatch(
-      {
-        #c.index = 1:length(unlist(inputSchema()))
-        
-        mapply(responsive.Widget, 
-               #       c.index,
-               unlist(inputSchema()[, "Type"]),
-               unlist(inputSchema()[, "Display"]),
-               unlist(inputSchema()[, "Field_Name"]))
-      }, 
-      error = function(cond) {
-        # 
-      })
-  })
-  
-  
-  output$theContract = renderText({
-    paste0(outputText(), collapse = "</br>")
-  })
-  
-  
-  #bus = reactive({
-  #  req(input$csvUpload)
-  #      nom = unlist(inputSchema()[, "Field_Name"])[2]
-  #  tryCatch(
-  #    {
-  #      get(x = nom, pos = input)
-  #    }, 
-  #    error = function(cond) {
-  #      # 
-  #    })
-  #})
-  
-  #busName = reactive({
-  #  req(input$csvUpload)
-  #  input$Business_Name
-  #  # get(unlist(inputSchema()[, "Field_Name"])[2], pos = input)
-  #})
-  
-  #fieldNames = reactive({
-  #  req(input$csvUpload)
-  #  unlist(inputSchema()[, "Field_Name"])
-  #})
-  
-  
+    output$csvTitle = renderText({
+        "<h3>Input Schema:</h3>"
+    })
+    
+    output$widgetsTitle = renderText({
+        "<h3>Contract Widgets:</h3>"
+    })
+    
+    output$outputTitle = renderText({
+        "<h3>Contract Output:</h3>"
+    })
+    
+    output$blankSpace = renderText({
+        "<h3>  </h3>"
+    })
+    
+    
+    # 
+    # Reactive values ###############################
+    # 
+    
+     
+    # Import user uploaded file (csvUpload is ID of input button).
+    inputSchema = reactive({
+        read.csv(file = input$csvUpload$datapath)
+    })
+    
+    # Get list of input values from Shiny, for the dynamic form.
     list_of_inputs = reactive({ 
         reactiveValuesToList(input)
     })
     
-    
+    # Get list of field names from the uploaded schema.
     list_of_fields = reactive({ 
         req(input$csvUpload)
         inputSchema()[, "Field_Name"]
     })
-
-  
-    #output$testIt = renderText({
-    #    # input$Business_Name
-    #    # req(input$csvUpload)
-    #    t.out = unlist(list_of_inputs())
-    #    #paste0(t.out[6], " ", t.out[1])
-    #    t.index = unlist(list_of_fields())
-    #    
-    #    t.out[t.index] # this works!
-    #})
+    
+    # Text output for preview before downloading. ## Needs work ##
+    outputText = reactive({
+        req(input$csvUpload)
+        t.out = unlist(list_of_inputs())
+        #paste0(t.out[6], " ", t.out[1])
+        t.index = unlist(list_of_fields())
+        
+        a = t.out[t.index] # this works!
+        b = unlist(mapply(contract.Text, 
+                          unlist(inputSchema()[, "Type"]), 
+                          unlist(inputSchema()[, "Display"]))) 
+        c(a, b)
+    })
     
     
-    #output$testIt = renderText({
-  #  req(input$csvUpload)
-  #  nom = unlist(inputSchema()[, "Field_Name"])[2]
-  #  get(x = nom, pos = input)
-  #})
-  
-  
+    # 
+    # Logic functions ###############################
+    # 
+    
+    # Export user created contract (downloadContract is ID of UI button).
+    output$downloadContract = downloadHandler(
+        filename = download.filename, 
+        # file is required by Shiny.
+        content = function(file) { 
+            write(paste0(outputText(), 
+                         sep = "\n"), 
+                  file = file) 
+        }
+    )
+    
+    # Generic widget type for use in UI. Currently only option is textInput().
+    responsive.Widget = function(type, display, id) {
+        switch (type,
+                qbox = textInput(inputId = id, label = display), 
+                title_text = NULL,
+                c_text = NULL
+        )
+    }
+    
+    # Helper to display text prior to download (can this be merged with ^)?
+    contract.Text = function(type, display) {
+        switch (type,
+                qbox = NULL, 
+                title_text = NULL,
+                c_text = display
+        )
+    }
+    
+    
+    # 
+    # Render functions ##############################
+    # 
+    
+    
+    # Display the user imported schema.
+    output$userInput = renderTable({
+        req(input$csvUpload)
+        read.csv(file = input$csvUpload$datapath)
+    })
+    
+    # Create dynamic user interface based on the imported schema.
+    output$theForm = renderUI({
+        tryCatch(
+            {
+                mapply(responsive.Widget, 
+                       unlist(inputSchema()[, "Type"]),
+                       unlist(inputSchema()[, "Display"]),
+                       unlist(inputSchema()[, "Field_Name"]))
+            }, 
+            error = function(cond) {
+                # Falls through if there's no schema uploaded.
+                # This is just to prevent "red text" on the UI. 
+                # No issues so far...
+            })
+    })
+    
+    # Dynamically display the text of the contract (this will be downloaded).
+    output$theContract = renderText({
+        paste0(outputText(), collapse = "</br>")
+    })
+    
+    
 })
-
-
-will.not.be.run = function() {
-  # for testing
-  
-  if (is.null(dim(input.data))) { 0 } else { dim(input.data[[1]]) }
-  tdat = as.data.frame(cbind(c(1, 2, 3), c("a", "b", "c")))
-  paste0(unlist(tdat[1, ]), collapse = "")
-  ?paste0
-}
-
-
-
-
-
